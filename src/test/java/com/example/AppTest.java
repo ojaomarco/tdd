1a719e767bc0f1d47eac9295f42971d3b70b886e
ojaomarco <84396808+ojaomarco@users.noreply.github.com>
2023-09-25 10:29:29 -0300

    update tests

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -4,29 +4,28 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest 
 {
 
     @Test
     public void shouldConvertKmToMiles(){
         UnitConverter uc = new UnitConverter();
-        boolean condition = uc.convertKmToMiles(10) == 16f;
+        boolean condition = uc.convertKmToMiles(10) == 6.2137f;
         assertTrue(condition);
     }
 
     @Test
     public void shouldConvertLbToKg(){
         UnitConverter uc = new UnitConverter();
-        boolean condition = uc.convertLbToKg(5.5f) == 10f;
+        boolean condition = uc.convertLbToKg(5.5f) == 24.9476f;
         assertTrue(condition);
     }
-
+   
+    @Test
     public void shouldConvertCelsiusToFarenheit(){
         UnitConverter uc = new UnitConverter();
-        boolean condition = uc.convertCelsiusToFh(1) == 73f;
+        boolean condition = uc.convertCelsiusToFh(32) == 89.6f;
         assertTrue(condition);
     }
 
